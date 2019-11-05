b2a8ad78f943703ee89746b5e6755be73c6a5d54
bernardogarza <bernardo.g.landa@gmail.com>
2019-11-05 14:38:03 -0600

    Working with local storage

--- src/partials/clear-form.js
+++ src/partials/clear-form.js
@@ -2,12 +2,16 @@ let title = document.querySelector('#title-project-input');
 let description = document.querySelector('#description');
 let date = document.querySelector('#date');
 let priority = document.querySelector('#priority');
+let taskRemove = document.querySelector('.new-task');
 
 let clearForm = () => {
   title.value = '';
   description.value = '';
   date.value = '';
   priority.value = '';
+  taskRemove.innerHTML = ''
+
+  
 }
 
 export { clearForm }
--- src/partials/create-task.js
+++ src/partials/create-task.js
@@ -1,9 +1,17 @@
 let createTaskButton = document.querySelector('.fa-plus');
+let newTask = document.querySelector('.new-task')
 
 let createTask = () => {
   createTaskButton.addEventListener('click', ()=> {
-    document.createElement('input', 'text');
+    let input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    input.classList.add('task-input')
+    newTask.appendChild(input);
   });
 }
 
 export {createTask}
+
+
+// var input = document.createElement("input");
+// input.setAttribute('type', 'text'); 
