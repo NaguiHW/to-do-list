1cfeda609f93bc0b53709b077071b9137a38ddce
Kalib Hackin <nagui08@hotmail.com>
2019-11-07 15:28:56 -0500

    working on the error message

--- src/partials/create-project-button.js
+++ src/partials/create-project-button.js
@@ -10,11 +10,18 @@ let title = document.querySelector('#title-project-input');
 let description = document.querySelector('#description');
 let date = document.querySelector('#date');
 let priority = document.querySelector('#priority');
+let errors = document.querySelector('.errors');
+
+let errorForm = () => {
+  let message = document.createElement('p');
+  message.innerHTML = 'Please fill the empty fields.'
+  errors.appendChild(message);
+}
 
 let createProject = () => {
   createProjectButton.addEventListener('click', (e) => {
     if (title.value === '' || description.value === '' || date.value === '' || priority.value === '') {
-      console.log('error');
+      errorForm();
     } else {
       storage();
       form.classList.add('hide')
