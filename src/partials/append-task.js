d912e67304a8fcef1d116116511c332b1e1210ef
bernardogarza <bernardo.g.landa@gmail.com>
2019-11-20 16:30:44 -0600

    deleted the update status file and added the function to the append task file

--- src/partials/append-task.js
+++ src/partials/append-task.js
@@ -1,6 +1,23 @@
 /* eslint-disable import/prefer-default-export */
 import { DataBase } from './storage';
-import { updateStatus } from './update-status';
+
+const updateStatus = (e, task) => {
+  const databaseProjects = DataBase.getProjects();
+  const projectIndex = e.parentElement.parentElement.parentElement.parentElement
+    .parentElement.parentElement.parentElement.cellIndex;
+  const taskIndex = e.parentElement.parentElement.parentElement.rowIndex;
+  if (databaseProjects[projectIndex].task[taskIndex].status) {
+    databaseProjects[projectIndex].task[taskIndex].status = false;
+    localStorage.setItem('project', JSON.stringify(databaseProjects));
+    task.classList.remove('true');
+    task.classList.add('false');
+  } else {
+    databaseProjects[projectIndex].task[taskIndex].status = true;
+    localStorage.setItem('project', JSON.stringify(databaseProjects));
+    task.classList.remove('false');
+    task.classList.add('true');
+  }
+};
 
 const taskBox = (newTask, taskArea) => {
   const task = document.createElement('div');
